64f279bdd286143e1da9b37e3fcb98c6f662488c
RickySandi <rickysandis@gmail.com>
2020-04-26 16:08:42 -0400

    Mnticulo Terminado

--- Monticulo/main.cpp
+++ Monticulo/main.cpp
@@ -4,142 +4,90 @@
 
 using namespace std;
 
-// void mostrarMenu() 
-// {
-//     Monticulo<string> arbol;
-//     fstream archEntrada;
-//     string palabra; 
-    
+void mostrarMenu() 
+{
+    Monticulo<int32_t> arbol;
 
-//     bool continuar = true;
-//     bool res; 
-
-//     int f1 = 0;
-//     int f2 =0;
-//     int f3 = 0; 
-
-//     int op;
-//     string v;
+    int op;
+    int v;
    
-//     do {
-//         cout << "-----MENU-----" << endl;
-//         cout << "1.Insertar valor" << endl;
-//         cout << "2.Recorrer IN Order" << endl;
-//         cout << "3.Recorrer PRE Order" << endl;
-//         cout << "4.Recorrer POST Order" << endl;
-//         cout << "5.Contar nodos" << endl;
-//         cout << "6.Calcular altura" << endl;
-//         cout << "7.Buscar" << endl;
-//         cout << "8.Encontrar mayor" << endl;
-//         cout << "9.Encontrar menor" << endl;
-//         cout<<  "10. Insertar libro"<<endl;
-//         cout<<"11. Nodos Factor"<<endl; 
-//         cout << "14 .Salir" << endl;
+    do {
+        cout << "-----MENU-----" << endl;
+        cout << "1.Insertar" << endl;
+        cout << "2.Eliminar raiz" << endl;
+        cout << "3.Recorrer IN Order" << endl;
+        cout << "4.Contar nodos" << endl;
+        cout << "5.Calcular altura" << endl;
+        cout << "6.Salir" << endl;
 
-//         cout << endl;
-//         cout << "Ingresa una opcion" << endl;
-//         cin >> op;
-//         switch (op)
-//         {
-//         case 1:
-//             cout << "Ingresa un valor para insertar" << endl;
-//             cin >> v;
-//             arbol.insertar(v,arbol.getRaiz(),continuar);
-//             break;
-//         case 2:
-//             cout << "Mostrando Arbol IN Order" << endl;
-//             arbol.mostrarInOrder(arbol.getRaiz());
-//             cout<<endl;
-//             break;
-//         case 3:
-//             cout << "Mostrando Arbol PRE Order" << endl;
-//             arbol.mostrarPreOrder(arbol.getRaiz());
-//             cout<<endl;
-//             break;
-//         case 4:
-//             cout << "Mostrando Arbol POST Order" << endl;
-//             arbol.mostrarPostOrder(arbol.getRaiz());
-//             cout<<endl;
-//             break;
-//         case 5:
-//             cout<<"El arbol tiene "<<arbol.contarNodos(arbol.getRaiz())<<" nodos"<<endl;
-//             break;
-
-//         case 6:
-//             cout<<"Altura: "<<arbol.altura(arbol.getRaiz())<<endl; 
-//             break;
-
-//         case 7:
-//             cout << "Ingresa un valor para buscar" << endl;
-//             cin>>v; 
-//             if(arbol.buscar(v, arbol.getRaiz())){
-//                 cout<< "Elemento encontrado"<<endl;
-//             } else {
-//                 cout<< "Elemento no encontrado"<<endl;
-//             }
-//             break;
-//         case 8:
-//             cout<< "El mayor es "<<arbol.mayor(arbol.getRaiz())<<endl;
-//             break;
-//         case 9:
-//             cout<< "El menor es "<<arbol.menor(arbol.getRaiz())<<endl;
-//             break;
-//         case 10:
-//              archEntrada.open("soledad.txt");
-//             while (!archEntrada.eof() && archEntrada >> palabra)
-//             {
-//                 arbol.insertar(palabra,arbol.getRaiz(),continuar);
-                
-//             }
-//             archEntrada.close();
-//             break;
-//          case 11:  
-//              arbol.nodosFactor(arbol.getRaiz(),f1,f2,f3);  
-//              cout<<"Factor 0 "<<f1<<endl;
-//              cout<<"Factor 1 "<<f2<<endl;
-//              cout<<"Factor -1 "<<f3<<endl; 
-//             break;
+        cout << endl;
+        cout << "Ingresa una opcion" << endl;
+        cin >> op;
+        switch (op)
+        {
+        case 1:
+            cout << "Ingresa un valor para insertar" << endl;
+            cin >> v;
+            arbol.insertar(v);
+            break;
+        case 2:
+            cout << "Eliminando raiz" << endl;
+            arbol.eliminar();
+            cout << "Raiz eliminada" << endl;
+            break;
+        case 3:
+            cout << "Mostrando Arbol IN Order" << endl;
+            arbol.mostrarInOrder();
+            cout<<endl;
+            break;
+        case 4:
+            cout<<"Nodos: "<<arbol.contarNodos()<<endl; 
+            break;
+        case 5:
+            cout<<"Altura: "<<arbol.altura()<<endl; 
+            break;
  
-//         case 14:    
-//             break; 
+        case 6:    
+            break; 
 
-//         default:
-//             break;
-//         }
+        default:
+            break;
+        }
         
-//     } while (op != 11);
-// }
+    } while (op != 6);
+}
 
 
 int main()
 {
-   // mostrarMenu(); 
+    mostrarMenu(); 
 
-  Monticulo <int> arbol; 
+//   Monticulo <int> arbol; 
 
-  //cout<<"Antes de insertar"<<endl; 
-  arbol.insertar(85);
-  arbol.insertar(55);
-  arbol.insertar(70); 
-  arbol.insertar(33);
-  arbol.insertar(54);
-  arbol.insertar(68);
-//arbol.insertar(50); 
+//   // cout<<"Antes de insertar"<<endl; 
+//   arbol.insertar(85);
+//   arbol.insertar(55);
+//   arbol.insertar(70); 
+//   arbol.insertar(33);
+//   arbol.insertar(54);
+//   arbol.insertar(68);
+//   arbol.insertar(50); 
 
- // cout<<"Despues de insertar"<<endl;
-  arbol.mostrarInOrder();
-  cout<<endl; 
-  cout<<"Nodos: "<<arbol.contarNodos()<<endl; 
-  cout<<"Altura: "<<arbol.altura()<<endl; 
-  cout<<"-----------------------------------------------"<<endl;
-  arbol.eliminar();
-  arbol.eliminar();
-  arbol.eliminar();
-  arbol.mostrarInOrder();
-  cout<<endl; 
-  cout<<"Nodos: "<<arbol.contarNodos()<<endl; 
-  cout<<"Altura: "<<arbol.altura()<<endl; 
-  //cout<<"Despues de mostrar"<<endl;
+
+//  // cout<<"Despues de insertar"<<endl;
+//   arbol.mostrarInOrder();
+//   cout<<endl; 
+//   cout<<"Nodos: "<<arbol.contarNodos()<<endl; 
+//   cout<<"Altura: "<<arbol.altura()<<endl; 
+//   cout<<"-----------------------------------------------"<<endl;
+//   arbol.eliminar();
+//   arbol.eliminar();
+//   arbol.eliminar();
+//   arbol.mostrarInOrder();
+//   cout<<endl; 
+//   cout<<"Nodos: "<<arbol.contarNodos()<<endl; 
+//   cout<<"Altura: "<<arbol.altura()<<endl; 
+//   //cout<<"Despues de mostrar"<<endl;
 
     
 }
