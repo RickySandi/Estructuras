45349a55fa2ac3536334b930d7f6b67722f4e332
RickySandi <rickysandis@gmail.com>
2020-05-23 15:45:04 -0400

    Faltan algoritmos - Version funcionando

--- Dijkstra_Tarea_2/Grafo.h
+++ Dijkstra_Tarea_2/Grafo.h
@@ -34,157 +34,6 @@ Grafo(){
 		vec[i]->setDistancia(999999);
 	}
 	}
-	
-	void insertarArista(T vertice1, T vertice2){ //vector<int>vecRepeticiones
-	
-		vec[vertice1]->setNombre(vertice1);
-		vec[vertice1]->setExiste(true);
-		vec[vertice1]->insertarFinalLista8(vertice2);
-
-		vec[vertice2]->setNombre(vertice2);
-		vec[vertice2]->setExiste(true);
-		vec[vertice2]->insertarFinalLista8(vertice1);
-
-		//contando
-		//vecRepeticiones[vertice2] += 1; 	
-	
-	}
-
-	
-bool BFS(T origen, T destino)
-{
-	bool encontre=false;
-	vec[origen]->setMarca(true);
-	cola.insertBack(origen);
-	while (cola.getExiste() != false && encontre == false)
-	{
-		int i_esimo = 1;
-		Terna<T>*adyacente=new Terna<T>;
-		T vertice = cola.getPrimero()->getValue();
-		cola.deleteFirst();
-		adyacente = vec[vertice]->sacarSiguienteAdyacente(i_esimo);
-		while (adyacente != NULL && encontre == false)
-		{
-			if (vec[adyacente->getValue()]->getMarca() == false)
-			{
-				vec[adyacente->getValue()]->setPadre(vertice);
-				if (adyacente->getValue() == destino)
-					encontre = true;
-				else{
-					vec[adyacente->getValue()]->setMarca(true);
-					cola.insertBack(adyacente->getValue());
-				}
-			}
-			i_esimo++;
-			adyacente=vec[vertice]->sacarSiguienteAdyacente(i_esimo);
-		}
-	}
-	return encontre;
-}
-
-bool DFS(T origen, T destino)
-{
-	bool encontre = false;
-	vec[origen]->setMarca(true);
-	int i_esimo = 1;
-	Terna<T>*adyacente=vec[origen]->sacarSiguienteAdyacente(i_esimo);
-	while (adyacente != NULL)
-	{
-		if (encontre == false && vec[adyacente->getValue()]->getMarca() == false)
-		{
-			vec[adyacente->getValue()]->setPadre(origen);
-			if (adyacente->getValue() == destino)
-				encontre = true;
-			else
-				encontre=DFS(adyacente->getValue(), destino);
-
-		}
-		i_esimo++;
-		adyacente= vec[origen]->sacarSiguienteAdyacente(i_esimo);
-	}
-	return encontre;
-}
-
-void mostrarCamino(int origen, int destino)
-{
-	int aux = destino;
-	while (aux != origen)
-	{
-		cout << aux << " <- ";
-		aux = vec[aux]->getPadre();
-	}
-	cout << origen << endl;
-}
-
-
-int numeroVertices(){
-	int cont = 0; 
-	for (int i = 0; i < TAM; i++){
-		if (vec[i]->getExiste() != false){
-				cont++;
-	}
- }
-	return cont; 
-}		
-void eliminarArista(T v){
-	for (int i = 0; i < TAM; i++){
-		if (vec[i]->getNombre() == v){
-			vec[i]->eliminarArista(v); 
-	} 
- }
-}
-//Defensa Mostrar y nodo que mas veces es destino 
-void mostrarGrafo()
-{
-	for (int i = 0; i < TAM; i++)
-	{
-		if (vec[i]->getExiste() != false)
-		{
-			cout << "Vertice: "<<vec[i]->getNombre() << endl;
-			cout << "Adyacentes:" << endl;
-			vec[i]->mostrarLista8();
-		}
-	}
-}
-// void Dijkstra(int origen)
-// {
-// 	int vertice, distancia;
-// 	Terna<int> *Ady;
-// 	 priority_queue< iPair, vector <iPair> , greater<iPair> > pq; 
-// 	vec[origen]->setDistancia(0);
-	
-// 	i.first = vec[origen].getDistancia();
-// 	pq.second.first = origen;
-// 	cola.push(pq);
-// 	while (!cola.empty())
-// 	{
-// 		pq = cola.top();
-// 		cola.pop();
-// 		vertice = pq.second.first;
-// 		if (vec[vertice].getMarca() == false)
-// 		{
-// 			vec[vertice].setMarca(true);
-// 			Ady = vec[vertice].getList()->getPrimero();
-// 			while (Ady != NULL)
-// 			{
-// 				if (vec[Ady->getValue()].getMarca() == false && Ady->getPeso()>0)
-// 				{
-// 					if (vec	[vertice].getDistancia() + Ady->getPeso()<vec[Ady->getValue()].getDistancia())
-// 					{
-// 						vec[Ady->getValue()].setDistancia(vec[vertice].getDistancia() + Ady->getPeso());
-// 						vec[Ady->getValue()].setPadre(vertice);
-// 						pq.first = vec[Ady->getValue()].getDistancia();
-// 						pq.second.first = Ady->getValue();
-// 						pq.second.second = 0;
-// 						cola.push(pq);
-// 					}
-// 				}
-// 				Ady = Ady->getNext();
-// 			}
-// 		}
-
-// 	}
-// }
 // Algoritmo para introducir datos en el GRAFO y distancias
 void llenarGrafo(map<string,vector<pair<string,int> > > &grafo, map<string,int> &dis,int conex)
 {
@@ -211,6 +60,7 @@ void MostrarGrafo(map<string,vector<pair<string,int> > > grafo,map<string,int> d
     map <string,vector < pair <string,int> > >::iterator iter;
     map <string,int> ::iterator itd;
     //Muestra grafo
+	cout<<"Mostrado Grafo"<<endl; 
     for(iter=grafo.begin();iter!=grafo.end();iter++)
     {
          cout<<endl<<(*iter).first<<" : "<<endl;
@@ -218,9 +68,11 @@ void MostrarGrafo(map<string,vector<pair<string,int> > > grafo,map<string,int> d
             cout<<" " << (*iter).second[c].first<<"/" <<(*iter).second[c].second<<", ";
     }
     //Muestra distancias
+	cout<<"Mostrado Distancias"<<endl;
     for(itd=dis.begin();itd!=dis.end();itd++)
            cout<<(*itd).first<<" = "<<(*itd).second<<endl;
 }
+
 
 // Dikjstra Entrada: GRAFO y Salida: distancias
 void Dikjstra(map<string,vector<pair<string,int> > > grafo,map<string,int> &distancias, string ori)
@@ -242,6 +94,31 @@ void Dikjstra(map<string,vector<pair<string,int> > > grafo,map<string,int> &dist
         cola.pop();
     }
 }
+void mostrarCamino(char origen, char destino)    
+{
+	char aux = destino;
+	int pos =aux-65; 
+	while (aux != origen)
+	{
+		cout << aux << " <- ";
+		aux = vec[pos]->getPadre();
+	}
+	cout << origen << endl;
+}
+		
+void eliminarArista(T v){
+	for (int i = 0; i < TAM; i++){
+		if (vec[i]->getNombre() == v){
+			vec[i]->eliminarArista(v); 
+	} 
+ }
+}
+
+
+
+
+
+
 
 
 };
--- Dijkstra_Tarea_2/main.cpp
+++ Dijkstra_Tarea_2/main.cpp
@@ -12,120 +12,98 @@ using namespace std;
 
 void mostrarMenu() 
 {
-   Grafo<int>* grafo = new Grafo<int>(); //char 
-   int origen, destino;
-   int arista;
-   fstream archEntrada; 
-    
-  
+//    Grafo<int>* grafo = new Grafo<int>(); //char 
+//    int origen, destino;
+//    int arista;
+//    fstream archEntrada; 
+    map <string,vector < pair <string,int> > > grafo;
+	Grafo<int>* grafo2 = new Grafo<int>(); 
+    map <string,int> distancias;
+    int conex;
+    string ori,dest;
+	char origen2, destino2; 
 
    int op;
 	do
 	{
 		cout << "---MENU---"<<endl;
-		cout << "1. Busqueda Amplitud - BFS"<<endl;
-		cout << "2. Busqueda Profundidad - DFS"<<endl;
-		cout << "3. Insertar Arista"<<endl;
-		cout << "4. Eliminar Arista"<<endl;
-		cout << "5. Definir numero de Vertices"<<endl;
-		cout << "6. Mostrar camino"<<endl;  //Para mostrar el camino, primero se lo debe buscar!!
+		cout << "1. Insertar Arista"<<endl;
+		cout << "2. Eliminar Arista - (FALTA)"<<endl;
+		cout << "3. Mostrar Lista Adyacencia"<<endl;
+		cout << "4. Dijkstra"<<endl;
+		cout << "5. Mostrar camino - (FALTA)"<<endl;  //Para mostrar el camino, primero se lo debe buscar!!
+		cout << "6.Salir"<<endl;
+
 		cout << "7. Inicializar Padre y Marca"<<endl;
 		cout << "8. Mostrar Grafo"<<endl;
 		cout << "9. Insertar archivo"<<endl;
 		cout<<  "10. Dijkstra"<<endl; 
-		cout << "11.Salir"<<endl;
+		
 		
 		cout<<"Ingresa una opcion"<<endl; 
 		cin >> op;
 		
 		switch (op)
 		{
+		
 		case 1:
-			cout << "Ingrese el origen: "<<endl;
-			cin >> origen;
-			cout << "Ingrese el destino: "<<endl;
-			cin >> destino;
-			grafo->inicializar();
-			if (grafo->BFS(origen, destino))
-				cout << "Camino Encontrado" << endl;
-			else
-				cout << "Camino no existe" << endl;
+		cout<<"Introduce el numero de aristas ";
+    	cin>>conex;
+    	grafo2->llenarGrafo(grafo,distancias,conex);
 			break;
-		case 2:
-			cout << "Ingrese el origen: "<<endl; // optimizar en una funcion 
-			cin >> origen;
-			cout << "Ingrese el destino: "<<endl;
-			cin >> destino;
-			grafo->inicializar();
-			if (grafo->DFS(origen, destino))
-				cout << "Camino encontrado"<<endl;
-			else
-				cout << "Camino no encontrado"<<endl;
-			break;
+		// case 2:
+		// 	cout<<"Ingresa la arista a eliminar"<<endl;
+		// 	cin>>arista;
+		// 	grafo->eliminarArista(arista);
+		// 	break;
 		case 3:
-			cout << "Ingrese el origen: "<<endl;
-			cin >> origen; //origen 
-			cout << "Ingrese el destino: "<<endl;
-			cin >> destino; // destino 
-			grafo->insertarArista(origen, destino);
+			grafo2->MostrarGrafo(grafo, distancias);
 			break;
 		case 4:
-			cout<<"Ingresa la arista a eliminar"<<endl;
-			cin>>arista;
-			grafo->eliminarArista(arista);
+  			cout<<"Introduce el nodo ORIGEN ";
+    		cin>>ori;
+    		grafo2->Dikjstra(grafo,distancias,ori);
 			break;
 		case 5:
-			cout << "El grafo tiene "<<grafo->numeroVertices()<<"vertices"<<endl;
+			cout<<"Ingresa el origen y destino"<<endl; 
+			cin>>ori>>dest; 
+			grafo2->mostrarCamino(origen2,destino2);
 			break;
-		case 6:
-			cout << "Ingrese el origen: "<<endl;
-			cin >> origen; //origen 
-			cout << "Ingrese el destino: "<<endl;
-			cin >> destino; // destino 
-			grafo->mostrarCamino(origen, destino); 
-			break;
-		case 7:
-			grafo->inicializar();
-			cout<<"Padre y Marca inicializados"<<endl; 
-			break;
-		case 8:
-			grafo->mostrarGrafo(); 
-			break; 
-		case 9:
-             archEntrada.open("grafo.txt");
-            while (!archEntrada.eof() && archEntrada >> origen >> destino)
-            {  //grafo->inicializarVector(vecRepeticiones); 
-               grafo->insertarArista(origen, destino);
-            }
-            archEntrada.close();
-		case 10:
-			cout << "Ingrese el origen: "<<endl;
-			cin >> origen; //origen 
-			//grafo->Dijkstra(origen);
+		
+		// case 9:
+        //      archEntrada.open("grafo.txt");
+        //     while (!archEntrada.eof() && archEntrada >> origen >> destino)
+        //     {  //grafo->inicializarVector(vecRepeticiones); 
+        //        grafo->insertarArista(origen, destino);
+        //     }
+        //     archEntrada.close();
 		}
 		
-	} while (op != 11);
+	} while (op != 6);
 }
 
 int main(){
 
-    //mostrarMenu();
-    map <string,vector < pair <string,int> > > grafo;
-	Grafo<int>* grafo2 = new Grafo<int>(); 
-    map <string,int> distancias;
-    int conex;
-    string ori;
+       mostrarMenu();
 
-    cout<<"Introduce el numero de aristas ";
-    cin>>conex;
-    grafo2->llenarGrafo(grafo,distancias,conex);
 
-    cout<<"Introduce el nodo ORIGEN ";
-    cin>>ori;
-    grafo2->Dikjstra(grafo,distancias,ori);
+    // map <string,vector < pair <string,int> > > grafo;
+	// Grafo<int>* grafo2 = new Grafo<int>(); 
+    // map <string,int> distancias;
+    // int conex;
+    // string ori;
 
-    cout<<"el proceso devuelve "<<endl;
-    grafo2->MostrarGrafo(grafo, distancias);
+    // cout<<"Introduce el numero de aristas ";
+    // cin>>conex;
+    // grafo2->llenarGrafo(grafo,distancias,conex);
+
+    // cout<<"Introduce el nodo ORIGEN ";
+    // cin>>ori;
+    // grafo2->Dikjstra(grafo,distancias,ori);
+
+    // cout<<"el proceso devuelve "<<endl;
+    // grafo2->MostrarGrafo(grafo, distancias);
+
 
 
 
